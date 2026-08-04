e60bcc79f6903f8c007921231feeb780089819cb
mathisarends <mathisarends27@gmail.com>
2026-08-04 07:01:50 +0200

    temp: remove cli

--- cli/src/huerise_cli/generated/models/audio_output.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class AudioOutput(str, Enum):
-    LOCAL = "local"
-    SONOS = "sonos"
-
-    def __str__(self) -> str:
-        return str(self.value)
--- cli/src/huerise_cli/generated/models/sound_category.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class SoundCategory(str, Enum):
-    GET_UP = "get_up"
-    WAKE_UP = "wake_up"
-
-    def __str__(self) -> str:
-        return str(self.value)
